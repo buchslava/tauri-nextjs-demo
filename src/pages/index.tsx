a3fd97b41c20a882cb259a9004c8c9e2003891fd
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2023-05-03 15:12:34 +0300

    add timer

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,20 +14,25 @@ interface ProgressEventProps {
 export default function Home() {
   const [busy, setBusy] = useState<boolean>(false);
   const [progress, setProgress] = useState<number>(0);
+  const [timeLabel, setTimeLabel] = useState<string>();
 
   useEffect(() => {
+    const timeIntervalId = setInterval(() => {
+      setTimeLabel(new Date().toLocaleTimeString());
+    }, 1000);
     const unListen = listen("PROGRESS", (e: ProgressEventProps) => {
       setProgress(e.payload.progress);
     });
 
     return () => {
+      clearInterval(timeIntervalId);
       unListen.then((f) => f());
     };
   }, []);
 
   return (
     <div>
-      {/* <div style={{ position: "fixed", top: 20, left: 20 }}>foo</div> */}
+      <div style={{ position: "fixed", top: 20, left: 20 }}>{timeLabel}</div>
       <div style={{ width: "70vw" }}>
         <Progress percent={progress} />
       </div>
