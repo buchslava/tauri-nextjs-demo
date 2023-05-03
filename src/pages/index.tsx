56b7b58b433f7d490bacd58545f208cb75acce19
Vyacheslav Chub <vyacheslav.chub@valor-software.com>
2023-05-03 14:58:23 +0300

    first version with progress bar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { invoke } from "@tauri-apps/api/tauri";
 import { listen } from "@tauri-apps/api/event";
 import { useEffect, useState } from "react";
+import { Button, Progress } from "antd";
 
 interface ProgressEventPayload {
   progress: number;
@@ -12,10 +13,11 @@ interface ProgressEventProps {
 
 export default function Home() {
   const [busy, setBusy] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(0);
 
   useEffect(() => {
     const unListen = listen("PROGRESS", (e: ProgressEventProps) => {
-      console.log(e.payload.progress);
+      setProgress(e.payload.progress);
     });
 
     return () => {
@@ -25,22 +27,26 @@ export default function Home() {
 
   return (
     <div>
-      {!busy && (
-        <button
-          onClick={() => {
-            setBusy(true);
-            setTimeout(async () => {
-              const { appWindow } = await import("@tauri-apps/api/window");
-              await invoke("progress_tracker", {
-                window: appWindow,
-              });
-              setBusy(false);
-            }, 1000);
-          }}
-        >
-          Start Progress
-        </button>
-      )}
+      {/* <div style={{ position: "fixed", top: 20, left: 20 }}>foo</div> */}
+      <div style={{ width: "70vw" }}>
+        <Progress percent={progress} />
+      </div>
+      <Button
+        type="primary"
+        disabled={busy}
+        onClick={() => {
+          setBusy(true);
+          setTimeout(async () => {
+            const { appWindow } = await import("@tauri-apps/api/window");
+            await invoke("progress_tracker", {
+              window: appWindow,
+            });
+            setBusy(false);
+          }, 1000);
+        }}
+      >
+        Start Progress
+      </Button>
     </div>
   );
 }
